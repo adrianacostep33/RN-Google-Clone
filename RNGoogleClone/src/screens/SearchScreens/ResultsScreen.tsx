d7742a902f340fa668c06e636fa2ec54f1c7678d
adrianacostep33 <adriana.coste@principal33.com>
2024-04-06 18:27:50 +0300

    Fetch data from firestore for testing

--- RNGoogleClone/src/screens/SearchScreens/ResultsScreen.tsx
+++ RNGoogleClone/src/screens/SearchScreens/ResultsScreen.tsx
@@ -1,8 +1,38 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Footer from '../../components/SearchScreen/Footer';
+import {Result} from '../../constants/Interfaces';
+
+import firestore from '@react-native-firebase/firestore';
 
 const ResultsScreen = () => {
+  // const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState<Result[]>([]);
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('results')
+      .onSnapshot(querySnapshot => {
+        const results: Result[] = [];
+
+        querySnapshot.forEach(documentSnapshot => {
+          results.push({
+            ...documentSnapshot.data(),
+            id: documentSnapshot.id,
+          });
+        });
+
+        console.log(results);
+
+        setResults(results);
+        // setLoading(false);
+      });
+
+    return () => subscriber();
+  }, []);
+
+  console.log({results});
+
   return (
     <View style={styles.container}>
       <Text>ResultsScreen</Text>
