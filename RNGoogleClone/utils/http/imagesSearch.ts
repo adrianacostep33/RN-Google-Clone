5436f053203240012c35af3acc1ae90c1f99f96b
adrianacostep33 <adriana.coste@principal33.com>
2024-04-08 13:14:38 +0300

    Add images search function

---
+++ RNGoogleClone/utils/http/imagesSearch.ts
@@ -0,0 +1,44 @@
+import {Result} from '../../src/constants/Interfaces';
+import firestore from '@react-native-firebase/firestore';
+
+const isDuplicate = (newDocument: Result, documents: Result[]) => {
+  return documents.some(doc => {
+    doc.description === newDocument.description;
+  });
+};
+
+const imagesSearch = async (searchValue: string[]) => {
+  const results: Result[] = [];
+  let counter = 0;
+  for (const element of searchValue) {
+    console.log({element});
+
+    firestore()
+      .collection('results')
+      .where('searchableDescription', 'array-contains', element.toLowerCase())
+      .where('image', '!=', '')
+      .onSnapshot(querySnapshot => {
+        querySnapshot.forEach(documentSnapshot => {
+          const data = documentSnapshot.data();
+          counter++;
+          console.log('Render number:', {counter}, {data});
+
+          const newDocument = {
+            id: documentSnapshot.id,
+            url: data.url,
+            title: data.title,
+            name: data.name,
+            description: data.description,
+            image: data.image,
+          };
+
+          if (isDuplicate(newDocument, results)) return;
+
+          results.push(newDocument);
+        });
+      });
+  }
+  return results;
+};
+
+export default imagesSearch;
